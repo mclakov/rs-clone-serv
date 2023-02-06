04adf8bd04648fc56962fbb12534dfd6e6a06d19
Roman Maklakov <mclakov@gmail.com>
2023-02-06 13:44:42 +0300

    fix: router

---
+++ appController.js
@@ -0,0 +1,19 @@
+class appController {
+    async reg(req, res) {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+
+    async login(req, res) {
+
+    }
+
+    async getUsers(req, res) {
+
+    }
+}
+
+module.exports = new appController();
--- appRouter.js
+++ appRouter.js
@@ -1,8 +1,9 @@
 const Router = require("express")
 const router = new Router()
+const controller = require("./appController")
 
-router.post("/reg");
-router.post("/login");
-router.get("/users");
+router.post("/reg", controller.reg);
+router.post("/login", controller.login);
+router.get("/users", controller.getUsers);
 
 module.exports = router;
