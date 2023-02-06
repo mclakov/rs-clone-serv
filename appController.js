4117f3e277e8ea42abb2c39d1993851a399fd2d8
Roman Maklakov <mclakov@gmail.com>
2023-02-06 15:20:18 +0300

    feat: add try catch block for error

--- appController.js
+++ appController.js
@@ -1,18 +1,31 @@
+const User = require("./models/User");
+
 class appController {
     async reg(req, res) {
         try {
 
         } catch (e) {
-
+            console.log(e);
+            res.status(400).json({message: "Registration error"})
         }
     }
 
     async login(req, res) {
+        try {
 
+        } catch (e) {
+            console.log(e);
+            res.status(400).json({message: "Login error"})
+        }
     }
 
     async getUsers(req, res) {
-        res.json("server work, getUsers()")
+        try {
+
+        } catch (e) {
+            console.log(e);
+            res.status(400).json({message: "Server error"})
+        }
     }
 }
 
