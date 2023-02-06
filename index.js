5edc64ddbdd3c6757d46d69e5364b7efa7a841eb
Roman Maklakov <mclakov@gmail.com>
2023-02-06 12:53:27 +0300

    feat: add mongoDB

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3008
 
 
 const app = express()
 
 
-const start = () => {
+const start = async () => {
   try {
+      await mongoose.connect("mongodb+srv://admin:admin123@cluster0.aehdvje.mongodb.net/clon-serv?retryWrites=true&w=majority")
       app.listen(PORT, () => (`server started on port ${PORT}`))
   } catch (e) {
       console.log(e)
